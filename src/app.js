cfea511df29769fe6481dc76e8f098ee35c152a5
Sebastian Cascante <139644093+devSebaKsk@users.noreply.github.com>
2025-02-04 03:59:14 +0000

    final

--- src/app.js
+++ src/app.js
@@ -5,32 +5,49 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let palo = ["spade", "club", "heart", "diamond"];
+let numero = [
+  "A",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
 window.onload = function() {
   //write your code here
-  let palo = ["spade", "club", "heart", "diamond"];
-  let numero = [
-    "A",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
+
   let nPalo = Math.floor(Math.random() * 4);
   let rndnumero = Math.floor(Math.random() * 13);
 
-  let parafo = document.createElement("p");
+  let parafo = document.createElement("div");
   parafo.innerHTML = numero[rndnumero];
   document.getElementById("card").appendChild(parafo);
   let classPalo = palo[nPalo];
   const list = document.getElementById("card").classList;
   list.add(classPalo);
+};
+
+//let boton = document.getElementsByClassName("button");
+//document.addEventListener("click", myFunction);
+
+window.myFunction = function myFunction() {
+  let nPalo = Math.floor(Math.random() * 4);
+  let rndnumero = Math.floor(Math.random() * 13);
+  let carta = document.createElement("div");
+  let classPalo = palo[nPalo];
+  let clase = "card " + classPalo;
+  carta.className = clase;
+  //card.setAttribute("class", classPalo);
+  carta.innerHTML = "<div>" + numero[rndnumero] + "</div>";
+  document.getElementById("mesa").appendChild(carta);
 };
